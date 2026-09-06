535c72b645ac0e56097eb8f62fcb1227fbb361c0
eymencanseven13 <eymencanseven13@gmail.com>
2026-09-06 11:34:34 +0300

    chore: update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,13 @@
 add_rules("mode.release")
-add_requires("preloader_android")
+add_requires("levilamina")
 target("MadenRadar")
     set_kind("shared")
     set_languages("cxx20")
     add_files("src/*.cpp")
-    add_packages("preloader_android")
+    add_packages("levilamina")
     set_filename("libMadenRadar.so")
     if is_mode("release") then
         set_strip("all")
-        add_cxxflags("-fvisibility=hidden")
+        add_cxxflags("-fvisibility=hidden","-ffunction-sections","-fdata-sections")
+        add_ldflags("-Wl,--gc-sections")
     end
